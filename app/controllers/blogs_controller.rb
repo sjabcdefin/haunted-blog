47eacd69d0132ef03a8551d104c4baa216808a11
tomoka <tomoka.saiwaki@gmail.com>
2025-06-11 18:51:46 +0900

    Add authorize_blog_owner before_action to restrict access to blog owner
    ブログの編集・削除を作成者のみに制限するため。作成者以外がURLを直接入力して操作できないようにするため。

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
+  before_action :authorize_blog_owner, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -49,5 +50,11 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+  end
+
+  def authorize_blog_owner
+    return if @blog.user == current_user
+
+    raise ActiveRecord::RecordNotFound
   end
 end
