cd2c4bac5dfc87fa73ddc280b28896a4e5208b5a
tomoka <tomoka.saiwaki@gmail.com>
2025-06-14 12:49:23 +0900

    Restrict random eyecatch usage to premium users
    プレミアムユーザー以外がランダムアイキャッチ機能を利用しようとした場合に、新規作成画面・編集画面へリダイレクトさせるよう制御を追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,6 +6,7 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: %i[show edit update destroy]
   before_action :authorize_blog_owner, only: %i[edit update destroy]
   before_action :authorize_secret_blog_access, only: %i[show]
+  before_action :reject_random_eyecatch, only: %i[create update]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -20,6 +21,11 @@ class BlogsController < ApplicationController
   def edit; end
 
   def create
+    if @invalid_random_eyecatch
+      redirect_to new_blog_path
+      return
+    end
+
     @blog = current_user.blogs.new(blog_params)
 
     if @blog.save
@@ -30,6 +36,11 @@ class BlogsController < ApplicationController
   end
 
   def update
+    if @invalid_random_eyecatch
+      redirect_to edit_blog_path(@blog)
+      return
+    end
+
     if @blog.update(blog_params)
       redirect_to blog_url(@blog), notice: 'Blog was successfully updated.'
     else
@@ -63,5 +74,9 @@ class BlogsController < ApplicationController
     return unless @blog.secret && @blog.user != current_user
 
     raise ActiveRecord::RecordNotFound
+  end
+
+  def reject_random_eyecatch
+    @invalid_random_eyecatch = !current_user.premium && blog_params[:random_eyecatch]
   end
 end
