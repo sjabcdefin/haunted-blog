2c9e41bda62b4a3d80efaba83eea285b03ff0856
tomoka <tomoka.saiwaki@gmail.com>
2025-06-21 18:56:04 +0900

    Refactor random_eyecatch validation into blog_params method
    攻撃と思われる不正なパラメータの有無によって挙動を変更すると、内部的な実装を攻撃者に見透かされる可能性があるため、
    不正なパラメータを無効化し、ブログの作成・修正はできるように修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,6 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: %i[show edit update destroy]
   before_action :authorize_blog_owner, only: %i[edit update destroy]
   before_action :authorize_secret_blog_access, only: %i[show]
-  before_action :reject_random_eyecatch, only: %i[create update]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -21,11 +20,6 @@ class BlogsController < ApplicationController
   def edit; end
 
   def create
-    if @invalid_random_eyecatch
-      redirect_to new_blog_path
-      return
-    end
-
     @blog = current_user.blogs.new(blog_params)
 
     if @blog.save
@@ -36,11 +30,6 @@ class BlogsController < ApplicationController
   end
 
   def update
-    if @invalid_random_eyecatch
-      redirect_to edit_blog_path(@blog)
-      return
-    end
-
     if @blog.update(blog_params)
       redirect_to blog_url(@blog), notice: 'Blog was successfully updated.'
     else
@@ -61,7 +50,9 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    permitted_params = params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    permitted_params[:random_eyecatch] = '0' if !current_user.premium && permitted_params[:random_eyecatch]
+    permitted_params
   end
 
   def authorize_blog_owner
@@ -74,9 +65,5 @@ class BlogsController < ApplicationController
     return unless @blog.secret && @blog.user != current_user
 
     raise ActiveRecord::RecordNotFound
-  end
-
-  def reject_random_eyecatch
-    @invalid_random_eyecatch = !current_user.premium && blog_params[:random_eyecatch]
   end
 end
