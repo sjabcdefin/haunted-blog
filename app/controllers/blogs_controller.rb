2e76de17cb1649b42137aa8593c735afac870a77
tomoka <tomoka.saiwaki@gmail.com>
2025-07-04 08:08:22 +0900

    Remove redundant condition for secret blogs visible to current user in show action
    自分のブログであれば秘密かどうかに関わらず見られてよいため

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,7 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.where(secret: false).or(Blog.where(secret: true, user: current_user)).find(params[:id])
+    @blog = Blog.where(secret: false).or(Blog.where(user: current_user)).find(params[:id])
   end
 
   def new
